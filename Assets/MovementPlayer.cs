fdd55a7dbe1803cd9ed0323306aec8fb49d10a05
Barmvanic <moreljade71@gmail.com>
2024-03-07 12:49:49 +0100

    Add script movement

--- Assets/MovementPlayer.cs
+++ Assets/MovementPlayer.cs
@@ -7,12 +7,23 @@ public class MovementPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Movement();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
+
+    void Movement()
+    {
+        if (Input.Getkey(KeyCode.LeftArrow))
+        //si une touche est appuyee
+        {
+            transform.Translate(0.05f, 0, 0);
+
+        }
+    }
+
 }
