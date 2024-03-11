462476717c67cc36952b907c7e8c015c3d60fa44
JadeMorel <moreljade71@gmail.com>
2024-03-11 22:24:44 +0100

    JUMP OK + DOUBLEJUMP

--- Assets/MovementPlayer.cs
+++ Assets/MovementPlayer.cs
@@ -13,7 +13,8 @@ public class MovementPlayer : MonoBehaviour
 
     public Rigidbody2D rb;
 
-    public bool isJumping; 
+    public bool isJumping;
+    bool doubleJump; 
 
 
     
@@ -40,6 +41,12 @@ public class MovementPlayer : MonoBehaviour
         if (Input.GetButtonDown("Jump") && !isJumping) //p1 in the air whit the jump then the function will not work
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
+            doubleJump = true; //p1 jump while on the floor, double jump will be true 
+        }
+        else if (doubleJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump * 0.4f); //when doubleJump is true P1 will jump again & set at 40% of the first jump
+            doubleJump = false; //after the second jump the doubleJump will be false 
         }
     }
     private void OnCollisionEnter2D(Collision2D other) //hit different game object
