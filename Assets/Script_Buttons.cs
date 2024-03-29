8d7c266a6bd27adcd9070206331596d994110e8d
Barmvanic <moreljade71@gmail.com>
2024-03-29 13:42:11 +0100

    Inte main menu

---
+++ Assets/Script_Buttons.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_Buttons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start_Button()
+    {
+        SceneManager.LoadScene("SCN_PROTOBLUES");
+    }
+}
