c1a9cd3b7ecb1beb3dc0219743d599e68a514836
JadeMorel <moreljade71@gmail.com>
2024-05-14 17:06:40 +0200

    Add script cameracontroltrigger

---
+++ Assets/CameraControlTrigger.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEditor;
+
+public class CameraControlTrigger : MonoBehaviour
+{
+    public CustomInspectorObjects customInspectorObjects;
+}
+
+[System.Serializable]
+public class CustomInspectorObjects
+{
+    public bool swapCameras = false;
+    public bool panCameraOnContact = false;
+
+    [HideInInspector] public CinemachineVirtualCamera cameraOnLeft;
+    [HideInInspector] public CinemachineVirtualCamera cameraOnRight;
+
+    [HideInInspector] public PanDirection panDirection;
+    [HideInInspector] public float panDistance = 3f;
+    [HideInInspector] public float panTime = 0.35f;
+}
+
+public enum PanDirection // select which direction the camera will pan in the inspector 
+{
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+[CustomEditor(typeof(CameraControlTrigger))]
+public class MyScriptEditor : Editor
+{
+    CameraControlTrigger cameraControlTrigger;
+
+    private void OnEnable()
+    {
+        cameraControlTrigger = (CameraControlTrigger)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (cameraControlTrigger.customInspectorObjects.swapCameras)
+        {
+            cameraControlTrigger.customInspectorObjects.cameraOnLeft = EditorGUILayout.ObjectField("Camera on Left", cameraControlTrigger.customInspectorObjects.cameraOnLeft,
+                typeof(CinemachineVirtualCamera), true) as CinemachineVirtualCamera;
+
+            cameraControlTrigger.customInspectorObjects.cameraOnRight = EditorGUILayout.ObjectField("Camera on Right", cameraControlTrigger.customInspectorObjects.cameraOnRight,
+                typeof(CinemachineVirtualCamera), true) as CinemachineVirtualCamera;
+        }
+
+        if (cameraControlTrigger.customInspectorObjects.panCameraOnContact)
+        {
+            cameraControlTrigger.customInspectorObjects.panDirection = (PanDirection)EditorGUILayout.EnumPopup("Camera Pan Direction",
+                cameraControlTrigger.customInspectorObjects.panDirection);
+
+            cameraControlTrigger.customInspectorObjects.panDistance = EditorGUILayout.FloatField("Pan Distance", cameraControlTrigger.customInspectorObjects.panDistance);
+            cameraControlTrigger.customInspectorObjects.panTime = EditorGUILayout.FloatField("Pan Time", cameraControlTrigger.customInspectorObjects.panTime);
+
+        }
+
+        if (GUI.changed) // don't reset the inspector 
+        {
+            EditorUtility.SetDirty(cameraControlTrigger);
+        }
+    }
+}
+
