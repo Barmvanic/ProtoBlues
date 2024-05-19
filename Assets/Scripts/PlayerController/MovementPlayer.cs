52a161b60d391d25b645b4937ec52dd812690467
JadeMorel <moreljade71@gmail.com>
2024-05-20 00:29:42 +0200

    Animation player
    Qte changement en cours + clean et retag platformes

--- Assets/Scripts/PlayerController/MovementPlayer.cs
+++ Assets/Scripts/PlayerController/MovementPlayer.cs
@@ -14,19 +14,23 @@ public class MovementPlayer : MonoBehaviour
     private float Move;
 
     public Rigidbody2D rb;
-    Animator animator; 
+    [SerializeField] Animator animator; 
 
     //[SerializeField] Double_Jump Pieds;
     //bool doubleJump;
     bool facingRight = true;
-    bool isGrounded = false; 
+    bool isGrounded = false;
+    // private bool _jumping = false;
 
     //CHECKPOINT
     private Transform LastCheckpoint = null;
-    
-    
 
+    // LayerMask pour vérifier le sol
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    private float groundCheckRadius = 0.2f;
 
+    
 
 
     void Start()
@@ -53,8 +57,13 @@ public class MovementPlayer : MonoBehaviour
             Flip(); 
         }
 
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         
-
+        if (isGrounded)
+        {
+            animator.SetBool("isJumping", false);
+            
+        }
     }
 
     void Movement ()
@@ -69,18 +78,24 @@ public class MovementPlayer : MonoBehaviour
             animator.SetBool("isWalking", Mathf.Abs(Move) > 0.01f);
         }
 
-
         if (Input.GetButtonDown("Jump") && isGrounded /*&& !Pieds.isJumping*/) //p1 in the air whit the jump then the function will not work
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
-           /* doubleJump = true;*/ //p1 jump while on the floor, double jump will be true 
+            StartCoroutine(JumpTrigger());
+
+            /* doubleJump = true;*/ //p1 jump while on the floor, double jump will be true 
             GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
             isGrounded = false;
+            
+
+
         }
         else if (Input.GetButtonDown("Jump") /*&& doubleJump*/)
         {
             rb.velocity = new Vector2(rb.velocity.x, jump * 0.3f); //when doubleJump is true P1 will jump again & set at 40% of the first jump
-           /* doubleJump = false;*/ //after the second jump the doubleJump will be false 
+            /* doubleJump = false;*/ //after the second jump the doubleJump will be false 
+            animator.SetBool("DoubleJump", true);
+            StartCoroutine(DoubleJump());
         }
     }
 
@@ -114,5 +129,15 @@ public class MovementPlayer : MonoBehaviour
         LastCheckpoint = checkpointTransform; 
     }
 
+    IEnumerator JumpTrigger()
+    {
+        yield return new WaitForSeconds(0.1f);
+        animator.SetBool("isJumping", true);
+    }
 
+    IEnumerator DoubleJump()
+    {
+        yield return new WaitForSeconds(0.1f);
+        animator.SetBool("DoubleJump", false);
+    }
 }
