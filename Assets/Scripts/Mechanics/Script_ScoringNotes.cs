fa06a9f1e9ba999170288fa234ffdca427deedf1
Jade <moreljade71@gmail.com>
2024-05-23 15:15:55 +0200

    DEBUG SCORING

--- Assets/Scripts/Mechanics/Script_ScoringNotes.cs
+++ Assets/Scripts/Mechanics/Script_ScoringNotes.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Script_ScoringNotes : MonoBehaviour
-{
+//public class Script_ScoringNotes : MonoBehaviour
+//{
 
-    public Inventory_notes Notes; 
-    [SerializeField] TextMeshProUGUI textScore;
+//    public Inventory_notes Notes;
+//    [SerializeField] TextMeshProUGUI textScore;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Scoring();
-    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        Scoring();
+//    }
 
-    private void Scoring()
-    {
-        textScore.text = Notes.notes.ToString();
-    }
-}
+//    private void Scoring()
+//    {
+//        textScore.text = Notes.notes.ToString();
+//    }
+//}
