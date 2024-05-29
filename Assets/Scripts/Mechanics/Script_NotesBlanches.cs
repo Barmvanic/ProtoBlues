2df1bcbcd36c84b7cbb711a384f8fb9708a8360c
Jade <moreljade71@gmail.com>
2024-05-29 14:38:56 +0200

    Add greenBACK

--- Assets/Scripts/Mechanics/Script_NotesBlanches.cs
+++ Assets/Scripts/Mechanics/Script_NotesBlanches.cs
@@ -1,41 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Script_NotesBlanches : MonoBehaviour
 {
 
-
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] SpriteRenderer noterenderer;
     [SerializeField] Script_Timer timer;
-    public Inventory_notes nc; //notecount
+    bool showed = false;
 
     void Start()
     {
-
-    }
-
-
-    void Update()
-    {
-
-
+        scoreText.color = new Color(1f, 1f, 1f, 0f);
+        noterenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            showed = true; 
             timer.remainingTime += 10f;
             Debug.Log("+10");
             gameObject.GetComponent<Collider2D>().enabled = false;
-            // Incrémente la variable noteCount dans le script ChangeSceneQTE
-            GameManager.Instance.noteCount++;
-            Destroy(gameObject);
+
+            GameManager.Instance.noteCount++; // script changement de SCN QTE 
             Debug.Log("Note collected");
+
+            StartCoroutine(HideScoreText("+10s"));
+            noterenderer.color = new Color(1f, 1f, 1f, 0f);
+            Debug.Log("-15s");
+
+            
 
 
         }
+    }
+
+    IEnumerator HideScoreText(string textvar)
+    {
+        Debug.Log("show");
+        scoreText.text = textvar;
+        scoreText.color = new Color(0f, 1f, 0f, 1f);
+        yield return new WaitForSeconds(1f);
+        Debug.Log("hide");
+        scoreText.color = new Color(1f, 1f, 1f, 0f);
+
+        Destroy(gameObject);
+
+
     }
 }
 
